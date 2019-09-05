2167fc4ab2db62ddc6e04ed7fdaee2ef41f3d966
Melissa Palmer <melissa.palmer@gmail.com>
2019-09-05 20:53:07 +0200

    correct flags use for broken example

--- gencode.sh
+++ gencode.sh
@@ -1,4 +1,4 @@
 
-java  -jar openapi-generator-cli.jar generate -i param_test.yaml -g spring -o output-broken -c spring-config.json  -t templates
+java  -jar openapi-generator-cli.jar generate -i param_test.yaml -g spring -o output-broken -c spring-config.json
 
 java  -jar openapi-generator-cli.jar generate -i param_test.yaml -g spring -o output-working -c spring-config.json  -t templates
